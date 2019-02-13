56c10dceb768d7fcb70501de4028a4a7f8bdc8b0
Kallu609 <khirvola@gmail.com>
2019-02-13 20:09:26 +0200

    add rom and fontset load. tweak datatypes.

--- src/org/eioo/chip8/Emulator.kt
+++ src/org/eioo/chip8/Emulator.kt
@@ -1,57 +1,85 @@
 package org.eioo.chip8
 
 class Emulator {
-    private var delayTimer: UShort? = null            // 60Hz counts to zero
-    private var soundTimer: UShort? = null            // -//-
 
-    private var opcode: UShort? = null                // Current operation code
-    private val memory: UIntArray = UIntArray(4096)   // Memory
-    private val gfx: UIntArray = UIntArray(64 * 32)   // Graphics, 1 bit for each pixelMemory
-    private val key: UIntArray = UIntArray(16)        // Pressed keys
-    private val V: UShortArray = UShortArray(16)      // Registers (V0, V1, ... VE) VE: Carry flag
-    private val stack: UShortArray = UShortArray(16)  // Stack
-    private var sp: UInt? = null                      // Stack pointer
-    private var I: UShort? = null                     // Index register
-    private var pc: UShort? = null                    // Program counter (0x000 -> 0xFFF)
+    private val memory: UIntArray = UIntArray(4096) // Memory
+    private val gfx: IntArray = IntArray(64 * 32)   // Graphics, 1 bit for each pixelMemory
+    private val key: IntArray = IntArray(16)        // Pressed keys
+    private val V: ShortArray = ShortArray(16)      // Registers (V0, V1, ... VE) VE: Carry flag
+    private val stack: ShortArray = ShortArray(16)  // Stack
+    private var opcode: Short = 0                   // Current operation code
+    private var sp: Int = 0                         // Stack pointer
+    private var I: Short = 0                        // Index register
+    private var pc: Int = 0                         // Program counter (0x000 -> 0xFFF)
+    private var delayTimer: Short = 0               // 60Hz counts to zero
+    private var soundTimer: Short = 0               // -//-
 
-    var running: Boolean = false                      // State of machine
+    var running: Boolean = false                    // State of machine
     var drawFlag: Boolean = false
 
     init {
-        setupGraphics()
-        setupKeys()
+        reset()
     }
 
     fun start() {
         running = true
+        println("Emulator started")
     }
 
     fun stop() {
         running = false
+        println("Emulator stopped")
     }
 
-    private fun setupGraphics() {
-        for (i in 0..gfx.size) {
-            gfx[i] = 0u
-        }
-    }
+    private fun reset() {
+        pc = 0x200
+        opcode = 0
+        I = 0
+        sp = 0
+        delayTimer = 0
+        soundTimer = 0
 
-    private fun setupKeys() {
-        for (i in 0..key.size) {
-            key[i] = 0u
-        }
+        for (i in 0..(memory.size - 1)) memory[i] = 0u
+        for (i in 0..(V.size - 1)) V[i] = 0
+        for (i in 0..(stack.size - 1)) stack[i] = 0
+        for (i in 0..(gfx.size - 1)) gfx[i] = 0
+        for (i in 0..(key.size - 1)) key[i] = 0
+
+        loadFontset()
     }
 
     fun setKeys() {
         // TODO: Check for key presses/releases, preferably in another file
     }
 
+    private fun loadFontset() {
+        for (i in 0..79) memory[i] = FONTSET[i].toUInt()
+    }
+
     fun loadRom(romPath: String) {
-        println("Loading ROM from path: $romPath")
-        // TODO: Load rom
+        println("Loading ROM from path: \"$romPath\"")
+        val res = this::class.java.classLoader.getResource(romPath)
+
+        if (res == null) {
+            println("Rom not found")
+            return stop()
+        }
+
+        val fileBytes = res.readBytes()
+
+        for (i in 0..(fileBytes.size - 1)) {
+            memory[0x200 + i] = fileBytes[i].toUInt()
+        }
+
+        println("ROM loaded, byte size: ${fileBytes.size}")
     }
 
     fun emulateCycle() {
-        // TODO: Emulate cycle
+        opcode = (memory[pc] shl 8 or memory[pc + 1]).toShort()
+
+        // Decode Opcode
+        // Execute Opcode
+
+        // Update timers
     }
 }
--- src/org/eioo/chip8/Main.kt
+++ src/org/eioo/chip8/Main.kt
@@ -3,10 +3,10 @@ package org.eioo.chip8
 fun main() {
     val emu = Emulator()
     emu.start()
+    emu.loadRom("roms/pong.rom")
 
     while (emu.running) {
         emu.emulateCycle()
-        emu.loadRom("")
 
         if (emu.drawFlag) {
             println("Drawing gfx")
